1b66c95e6580f2bfaa0790acbf2ddacbbdd34a5f
LittleFish-Coder <adrianyu890317@gmail.com>
2024-05-25 21:13:22 +0800

    add net D

--- scripts/download_pretrained_weight.sh
+++ scripts/download_pretrained_weight.sh
@@ -2,8 +2,12 @@
 mkdir -p ./checkpoints/ROAD_RIVER_pix2pix
 
 # set the model file path
-MODEL_FILE=./checkpoints/ROAD_RIVER_pix2pix/latest_net_G.pth
-pth_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/EStoJ1cEWjtBsJqOH1v3JW8BxwEzca7_vPpWEkHfsP19Yw?e=u0ggoJ&download=1"
+NET_G_FILE=./checkpoints/ROAD_RIVER_pix2pix/latest_net_G.pth
+NET_D_FILE=./checkpoints/ROAD_RIVER_pix2pix/latest_net_D.pth
+net_G_url="https://ncku365-my.sharepoint.com/:f:/g/personal/nm6121030_ncku_edu_tw/Ep4F1YoBC2lHhmPUpATWqmYBUDYzw_GXS0dRB-Esr1zAMg?e=ZfLSao&download=1"
+net_D_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/EaVmCVx45qFAqdH4BtS-FxkBn4R_qzmJRM1m33M7f3AZcw?e=gnnOM4&download=1"
 
 # download the pretrained weight
-wget -N $pth_url -O $MODEL_FILE
+echo "Downloading the pretrained weight for the ROAD_RIVER_pix2pix model..."
+wget -N $net_G_url -O $NET_G_FILE
+wget -N $net_D_url -O $NET_D_FILE
